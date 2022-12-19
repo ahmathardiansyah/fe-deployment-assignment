6fda1265fbd5ef7db9db75d9afcbbd83dc2f0424
Ahmat Hardiansyah <ahmathardiansyah002@gmail.com>
2022-12-19 14:14:10 +0700

    new file

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "fe-deployment-assignment-fe2725147.netlify.app" // TODO: replace this
 const studentName = "AHMAT HARDIANSYAH" // TODO: replace this
 const studentId = "FE2725147" // TODO: replace this
 
